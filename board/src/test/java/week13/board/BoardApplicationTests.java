b972c3ead8759f303e398c93221ff39a6b72fc57
namhoon-kim97 <kskcmh5723@gmail.com>
2024-06-05 01:25:26 +0900

    init spring project

---
+++ board/src/test/java/week13/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package week13.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
