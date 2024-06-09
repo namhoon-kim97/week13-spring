49510e9e4ac8398ae1ef08150319e802b2ea161c
namhoon-kim97 <kskcmh5723@gmail.com>
2024-06-09 22:23:04 +0900

    refactor: login controller ApiResponse로 수정

--- src/main/java/week13/board/controller/AuthController.java
+++ src/main/java/week13/board/controller/AuthController.java
@@ -1,6 +1,7 @@
 package week13.board.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import week13.board.constants.ResponseMessage;
+import week13.board.dto.ApiResponse;
 import week13.board.dto.LoginDto;
 import week13.board.dto.TokenDto;
 import week13.board.jwt.JwtFilter;
@@ -19,18 +22,14 @@ import week13.board.jwt.TokenProvider;
 
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class AuthController {
     private final TokenProvider tokenProvider;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
-    public AuthController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
-        this.tokenProvider = tokenProvider;
-        this.authenticationManagerBuilder = authenticationManagerBuilder;
-    }
-
     @PostMapping("/authenticate")
-    public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto loginDto) {
+    public ResponseEntity<ApiResponse<TokenDto>> authorize(@Valid @RequestBody LoginDto loginDto) {
 
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
@@ -43,6 +42,10 @@ public class AuthController {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
 
-        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
+        ApiResponse<TokenDto> response = new ApiResponse<>(HttpStatus.OK.value(), ResponseMessage.USER_LOGIN_SUCCESS, new TokenDto(jwt));
+
+        return ResponseEntity.ok()
+                .headers(httpHeaders)
+                .body(response);
     }
 }
