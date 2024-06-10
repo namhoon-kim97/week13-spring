ebdeefd03597995d6842759c96e963eb0c16f494
namhoon-kim97 <kskcmh5723@gmail.com>
2024-06-10 21:40:29 +0900

    refactor: CommentController return ResponseEntity 형식 통일

--- src/main/java/week13/board/constants/ResponseMessage.java
+++ src/main/java/week13/board/constants/ResponseMessage.java
@@ -9,4 +9,7 @@ public class ResponseMessage {
     public static final String POST_DELETE_SUCCESS = "게시글 삭제 성공";
     public static final String USER_REGISTER_SUCCESS = "유저 회원가입 성공";
     public static final String USER_LOGIN_SUCCESS = "유저 로그인 성공";
+    public static final String COMMENT_CREATE_SUCCESS = "댓글 작성 성공";
+    public static final String COMMENT_UPDATE_SUCCESS = "댓글 수정 성공";
+    public static final String COMMENT_DELETE_SUCCESS = "댓글 삭제 성공";
 }
--- src/main/java/week13/board/controller/CommentController.java
+++ src/main/java/week13/board/controller/CommentController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import week13.board.constants.ResponseMessage;
+import week13.board.dto.ApiResponse;
 import week13.board.dto.CommentRequestDto;
 import week13.board.dto.CommentResponseDto;
 import week13.board.service.CommentService;
@@ -17,30 +19,33 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/{postId}")
-    public ResponseEntity<CommentResponseDto> createComment(@PathVariable Long postId, @RequestBody CommentRequestDto requestDto) {
+    public ResponseEntity<ApiResponse<CommentResponseDto>> createComment(@PathVariable Long postId, @RequestBody CommentRequestDto requestDto) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String currentUsername = authentication.getName();
 
         CommentResponseDto commentResponseDto = commentService.createComment(postId, currentUsername, requestDto);
-        return new ResponseEntity<>(commentResponseDto, HttpStatus.CREATED);
+        ApiResponse<CommentResponseDto> response = new ApiResponse<>(HttpStatus.CREATED.value(), ResponseMessage.COMMENT_CREATE_SUCCESS, commentResponseDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PutMapping("/{postId}/{commentId}")
-    public ResponseEntity<CommentResponseDto> updateComment(@PathVariable Long postId, @PathVariable Long commentId, @RequestBody CommentRequestDto requestDto) {
+    public ResponseEntity<ApiResponse<CommentResponseDto>> updateComment(@PathVariable Long postId, @PathVariable Long commentId, @RequestBody CommentRequestDto requestDto) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String currentUsername = authentication.getName();
 
         CommentResponseDto updatedCommentResponseDto = commentService.updateComment(postId, commentId, currentUsername, requestDto);
-        return new ResponseEntity<>(updatedCommentResponseDto, HttpStatus.OK);
+        ApiResponse<CommentResponseDto> response = new ApiResponse<>(HttpStatus.OK.value(), ResponseMessage.COMMENT_UPDATE_SUCCESS, updatedCommentResponseDto);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/{postId}/{commentId}")
-    public ResponseEntity<Void> deleteComment(@PathVariable Long postId, @PathVariable Long commentId) {
+    public ResponseEntity<ApiResponse<Void>> deleteComment(@PathVariable Long postId, @PathVariable Long commentId) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String currentUsername = authentication.getName();
 
         commentService.deleteComment(postId, commentId, currentUsername);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        ApiResponse<Void> response = new ApiResponse<>(HttpStatus.OK.value(), ResponseMessage.COMMENT_DELETE_SUCCESS, null);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
 
